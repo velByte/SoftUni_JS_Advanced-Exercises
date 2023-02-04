ddded6d9c21c6501fff93b5a3dbf6349d4ccbca9
velByte <67099502+velByte@users.noreply.github.com>
2023-02-04 07:57:36 +0100

    Update solution.js
    Added Solutions

--- Advanced Functions - Exercise/10. Central Cinema/solution.js
+++ Advanced Functions - Exercise/10. Central Cinema/solution.js
@@ -1,3 +1,30 @@
 function solve() {
-    // TODO
+    const addMovieSection = document.getElementById('container');
+    const moviesSection = document.getElementById('movies');
+    const archiveSection = document.getElementById('archive');
+    
+    const onScreenBtn = addMovieSection.getElementsByTagName('button')[0];
+    onScreenBtn.addEventListener('click', onScreenBtnClicked);
+
+    function onScreenBtnClicked(e){
+        e.preventDefault();
+
+        const [movieName, hallName, ticketPrice] = addMovieSection.querySelectorAll('input');
+        
+        if(
+            movieName.value.trim() === '' ||
+            hallName.value.trim() === '' || 
+            ticketPrice.value.trim() === '' || 
+            isNaN(ticketPrice.value.trim()) == true
+        ) {
+            return
+        } 
+
+
+        //Clear the input
+        movieName.value = '';
+        hallName.value = '';
+        ticketPrice.value = '';
+
+    }
 }
