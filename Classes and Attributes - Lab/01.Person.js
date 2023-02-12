4db9330210568c25fae249fea53e9b2ff7c120c0
velByte <67099502+velByte@users.noreply.github.com>
2023-02-12 06:42:47 +0100

    Added solution

---
+++ Classes and Attributes - Lab/01.Person.js
@@ -0,0 +1,21 @@
+function test(){
+    
+    class Person{
+        constructor(firstName, lastName, age, email){
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.age = age;
+            this.email = email
+        }
+
+        toString(){
+            return `${this.firstName} ${this.lastName} (age: ${this.age}, email: ${this.email})`
+        }
+    }
+
+
+    let persone = new Person('Anna', 'Simpson', 22, 'anna@yahoo.com')
+    console.log(persone.toString());
+}
+
+test()
